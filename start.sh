94544efa71dbdc34c79adf82cc60b1a43823e22d
Eric W. Greene <eric@ericwgreene.com>
2016-03-15 06:06:21 -0500

    minor tweaks

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-docker pull training4developers/node-oracle
+#docker pull training4developers/node-oracle
 
 docker run -i -t \
 	-p 49161:1521 -p 49160:3000 -p 49162:8080 \
-	-v `pwd`/app:/opt/app \
-	--entrypoint /bin/bash \
+	-v `pwd`/:/opt/app \
 	training4developers/node-oracle
+
+#	--entrypoint /bin/bash \
